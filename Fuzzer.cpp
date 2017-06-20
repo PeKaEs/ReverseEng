89b63c41fc885ad6d5a11ebd737e23b8d82a3d24
PeKaEs <pekaes@protonmail.ch>
2017-06-20 03:42:03 +0200

    End

--- Fuzzer.cpp
+++ Fuzzer.cpp
@@ -1,8 +1,8 @@
 #include "connection.hpp"
 
 void dump(unsigned char* buf, int size){
-  //for(int i = (size - 1) ; i>=0; --i){
-  for(int i = 0 ; i<size; ++i){
+  for(int i = (size - 1) ; i>=0; --i){
+  //for(int i = 0 ; i<size; ++i){
     printf("%02x",buf[i]);
     //printf("%02x",i);
   }
@@ -22,37 +22,60 @@ int main(int argc, char *argv[]){
 
 
   unsigned char buf[1024];
-  uint32_t in1 = 0x0000000f;
-  uint32_t in2 = 0x0000000a;
-  unsigned char option = 0xc6;
+  uint32_t in1 = 0x00000002; // 0x40490e99 PI   0x3fc90fdb PI/2  0x40c90fd0 PI*2
+  uint32_t in2 = 0x00000001;
+  uint32_t in3 = 0x00000001;
+  unsigned char option = 0x0b;
 
   unsigned char seq[] ={0xfe,0xed,0xfa,0xce,0xfe,0xed,0xfa,0xce};
+/*
   client c(host,port);
   //c.auth(239859);
   c.auth(210111);
 
-for (uint i = 0x0 ; i<=0xffffffff ; i+=0x01){
+  c.header(option);
+  c.write((unsigned char*)&in1,sizeof(in1));
+  c.ret();
+  if(c.read(buf,4)) {printf("Data for: 0x%04x,",in1);dump((unsigned char*)buf,4);}
+*/
+client c(host,port);
+//c.auth(239859);
+c.auth(210111);
+for (uint i = 0x00 ; i<=0xffffffff ; i+=0x01){
 {
-
+{
 
 
   c.header(option);
   c.write((unsigned char*)&in1,sizeof(in1));
+  c.header(option);
+  c.write((unsigned char*)&in2,sizeof(in1));
+  //c.header(option);
+  //c.write((unsigned char*)&in2,sizeof(in1));
+  //c.header(option);
+  //c.write((unsigned char*)&in2,sizeof(in1));
+
+  //c.header(option);
+  //c.write((unsigned char*)&in2,sizeof(in1));
+
+//  c.header(option);
+//  c.write((unsigned char*)&in3,sizeof(in1));
+
 
   //c.write(seq,sizeof(seq));
-  //i+=0xff;
+  //in1+=0x01;
   //i++;
   //c.header(option);
-  //c.write((unsigned char*)&in2,sizeof(i));
+  //c.write((unsigned char*)&in1,sizeof(i));
 
 
-if(i%2==0){
+if(1){
   c.ret();
 
-  if(c.read(buf,4)) {printf("\nData for: 0x%08x\n",i);dump((unsigned char*)buf,4);}
+  if(c.read(buf,4)) {printf("Data for: 0x%04x,",i);dump((unsigned char*)buf,4);}
     }
   }
-
+}
 }
 
 /*
@@ -60,11 +83,12 @@ for (unsigned char x=0; x<255 ; ++x){
   //printf("\nOption: 0x%02x\n",x);
   c.header(x);
   c.write((unsigned char*)&in1,sizeof(in1));
+  c.write((unsigned char*)&in2,sizeof(in1));
 
-  c.header(x);
-  c.write((unsigned char*)&in1,sizeof(in1));
-  c.ret2();
-  if(c.read(buf,4)) {printf("\nOption: 0x%02x\n",x);dump((unsigned char*)buf,4);}
+  //c.header(x);
+  //c.write((unsigned char*)&in1,sizeof(in1));
+  c.ret();
+  if(c.read(buf,4)) {printf("\nOption: 0x%02x : ",x);dump((unsigned char*)buf,4);}
 }
 */
 /*
@@ -72,8 +96,10 @@ for (unsigned char x=0; x<255 ; ++x){
     char buf[1024];
 
     char j = (i*174)%255;
-    //j=0x99;
-    c.write((unsigned char*)&j,sizeof(i));
+    j=i;
+    c.write((unsigned char*)&i,sizeof(i));
+    c.write((unsigned char*)&in2,sizeof(in1));
+
     //c.header();
 
 
